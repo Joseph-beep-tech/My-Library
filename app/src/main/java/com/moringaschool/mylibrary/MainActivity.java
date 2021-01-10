0c6ee12cf795fe185e5b0cd3b574ac4634c13135
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-10 14:22:59 +0300

    button functionality

--- app/src/main/java/com/moringaschool/mylibrary/MainActivity.java
+++ app/src/main/java/com/moringaschool/mylibrary/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,7 +20,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.findRegisterButton) Button mFindRegisterButton;
-//    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
@@ -26,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mFindRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == mFindRegisterButton){
+                    String location = mLocationEditText.getText().toString();
+
+                    Intent intent = new Intent(MainActivity.this, LibraryActivity.class);
+                    intent.putExtra("location", location);
+                }
+            }
+        });
     }
 
 
