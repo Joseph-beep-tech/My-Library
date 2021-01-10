56638dafef62f4deff48603196c0d1dae625c399
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-10 11:21:04 +0300

    removed files

--- app/src/main/java/com/moringaschool/mylibrary/MainActivity.java
+++ app/src/main/java/com/moringaschool/mylibrary/MainActivity.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.ViewPropertyAnimator;
 
 import java.util.ArrayList;
 
@@ -17,37 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-
-        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
-        pagerAdapter.addFragmet(new RegisterFragment());
-        viewPager.setAdapter(pagerAdapter);
-
     }
 
-    class AuthenticationPagerAdapter extends FragmentPagerAdapter{
-        private ArrayList<Fragment> fragmentList = new ArrayList<>();
 
-        public AuthenticationPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i){
-            return fragmentList.get(i);
-        }
-
-        @Override
-        public int getCount(){
-            return fragmentList.size();
-        }
-        void addFragment(Fragment fragment){
-            fragmentList.add(fragment);
-        }
-
-        public void addFragmet(RegisterFragment registerFragment) {
-
-        }
-    }
 }
